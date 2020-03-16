f823f07f41fa390e94f91fecc426557e56c911dc
GrafDrakula-BlaBlaBla <bogdanlevchenko11.l@gmail.com>
2020-03-16 19:34:39 +0300

    feat: add event for phone display

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 // Menu
-let menu = document.querySelector('.menu');
+let menu = document.querySelector(`.menu`);
 
-menu.addEventListener('click', event => {
+menu.addEventListener(`click`, event => {
     let menuItems = document.querySelectorAll(`.menu li a`);
     let target = event.target;
-    if (target.tagName.toLowerCase() != 'a') return;
+    if (target.tagName.toLowerCase() !== `a`) return;
     menuItems.forEach(item => {
-            item.classList.remove('active');
+            item.classList.remove(`active`);
     });
-    target.classList.add('active');
+    target.classList.add(`active`);
 });
+
+
+
+//Display
+document.querySelector(`.vertical`).addEventListener(`click`,(event) => {
+    // let target = event.target.closest(`.vertical`);
+    let figure = document.querySelector('.vertical .phone-display');
+    figure.classList.toggle('none');
+});
+
+document.querySelector(`.horizontal`).addEventListener(`click`,(event) => {
+    // let target = event.target.closest(`.horizontal`);
+    let figure = document.querySelector('.horizontal .phone-display');
+    figure.classList.toggle('none');
+});
+
+
+
+// Slider
+
 
